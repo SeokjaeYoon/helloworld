59cbff1cae9e3f892e9feb59fe835e95a0553979
Seokjae <cronos27@naver.com>
2017-07-12 17:34:09 +0900

    학습용 처음 업로드

---
+++ Assets/ReadKey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadKey : MonoBehaviour
+{
+    public Transform mySpriteTR;
+
+	void Start ()
+    {
+        
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            mySpriteTR.localPosition -= (Vector3.right * 0.04f);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            mySpriteTR.localPosition += (Vector3.right * 1.0f);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            mySpriteTR.localPosition += (Vector3.up * 0.2f);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            mySpriteTR.localPosition -= (Vector3.up * 1.0f);
+        }
+    }
+}
